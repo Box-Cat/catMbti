6dc91d63f74934dae740ad88eea23a45559e35c8
github Box-Cat <github pjh211pjh@gmail.com>
2022-05-08 23:10:32 +0900

    add route logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,18 @@ import React from 'react';
 import styled from 'styled-components';
 import PangImage from '../assets/cat1.png';
 import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+
+    const handleClickButton = () => {
+        //useHistory
+        navigate('/question');
+        
+    }
+
     return (
         <Wrapper>
             <Header>예비집사 판별기</Header>
@@ -14,7 +24,7 @@ const Home = () => {
                     <img src={PangImage} className="rounded-circle" width={350} height={350} />
                 </LogoImage>
                 <Desc>MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
-                <Button>테스트 시작하기</Button>
+                <Button style={{fontFamily: "SimKyungha"}} onClick={handleClickButton}>테스트 시작하기</Button>
             </Contents>
         </Wrapper>
     );
@@ -33,6 +43,7 @@ const Header = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-family: "SimKyungha";
 `
 
 const Contents = styled.div`
@@ -45,6 +56,7 @@ const Contents = styled.div`
 const Title = styled.div`
     font-size: 30pt;
     margin-top: 40px;
+    font-family: "SimKyungha";
 `
 
 const LogoImage = styled.div`
@@ -54,4 +66,5 @@ const LogoImage = styled.div`
 const Desc = styled.div`
     font-size: 20pt;
     margin-top: 20px;
+    font-family: "SimKyungha";
 `
